27d6374b69a2af55857b8faf80f6eec3094d9a11
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-02-08 07:17:52 +0900

    fix: 오타수정

--- bin/generate.js
+++ bin/generate.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 if (process.argv.length < 3) {
   console.log("You have to provide a name to your app.");
   console.log("For example :");
-  console.log("    npx create-react-odd my-app");
+  console.log("npx cra-swjs my-app");
   process.exit(1);
 }
 
