a4c4911ecd7e208cc725a44720c5113e882ffc9a
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-02-11 21:31:59 +0900

    fix : 오류 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = document.getElementById("root");
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
-ReactDOM.createRoot(root).render(
+root.render(
   <>
     <App />
   </>
